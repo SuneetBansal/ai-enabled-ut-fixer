6085b36904fdb37b3c383dd53341cd6f80d7363a
SuneetBansal <32817920+SuneetBansal@users.noreply.github.com>
2025-12-20 14:24:56 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('vitest');
+  protected readonly title = signal('vitest1');
 }
